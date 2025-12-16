93981e5a2768adfc3c60fc0043e7f51324da0ba1
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-16 16:47:02 +0530

    [Aman] : added Program for Volume of Cyclinder

---
+++ gcr-codebase/program-prerequisite/VolumeOfCyclinder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class VolumeOfCyclinder{
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Radius : ");
+		double radius = sc.nextDouble();
+		System.out.println("Enter Height : ");
+		int height = sc.nextInt();
+		
+		
+		
+		
+		
+		System.out.println( 3.14 * radius * radius * height );
+		
+	}
+}
