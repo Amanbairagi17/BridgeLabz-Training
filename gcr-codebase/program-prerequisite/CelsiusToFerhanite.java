7c2e26ec18d57eb734a48039dd5ab7c6f8d38c2f
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-16 16:40:58 +0530

    [Aman] : added Program of Celsius to Ferhanite

---
+++ gcr-codebase/program-prerequisite/CelsiusToFerhanite.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class CelsiusToFerhanite{
+	public static void main(String[] args){
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter value in Celsius");
+		int celsius = sc.nextInt();
+		System.out.println( (celsius * 9/5) + 32);
+		
+	}
+}
