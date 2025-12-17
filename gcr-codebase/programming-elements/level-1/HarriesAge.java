0d2f3f5b547f6fad83641d2fb79cd3eb17429701
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-17 11:52:14 +0530

    [Aman] : added program for Finding Age Of Harry

---
+++ gcr-codebase/programming-elements/level-1/HarriesAge.java
@@ -0,0 +1,11 @@
+public class HarriesAge{
+	public static void main(String[] args){
+		
+		int birthYearOfHarry = 2000;
+		int currentYearOfHarry = 2024;
+		int ageOfHarry = currentYearOfHarry - birthYearOfHarry;
+		System.out.println( "Current Age Of Harry : " +ageOfHarry );
+		
+		
+	}
+}
