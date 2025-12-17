f7028b1b3a92e2930c76f10cfa439ff349838600
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-17 19:42:16 +0530

    [Aman] : added updated program

--- gcr-codebase/program-prerequisite/KilometerToMiles.java
+++ gcr-codebase/program-prerequisite/KilometerToMiles.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class KilometerToMiles{
 	public static void main(String[] args){
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Kilometers : ");
-		
+
 		int kilometers = sc.nextInt();
-	
+
 		System.out.println(kilometers+" = "+ kilometers * 0.621371 +"Miles");
-		
+
 	}
 }
