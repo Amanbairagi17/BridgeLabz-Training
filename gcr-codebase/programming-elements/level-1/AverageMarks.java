172e599f99a3ea3d27477622f020faf70b479527
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-17 12:45:45 +0530

    [Aman] : added a program for find average marks

---
+++ gcr-codebase/programming-elements/level-1/AverageMarks.java
@@ -0,0 +1,22 @@
+public class AverageMarks{
+	public static void main(String[] args){
+		
+		// Marks of Sam in Maths 
+		int marksOfMaths = 94;
+		
+		//// Marks of Sam in Physics
+		int marksOfPhysics = 95;
+		
+		//// Marks of Sam in Chemisrty
+		int marksofChemistry = 96;
+		
+		
+		// Average marks of Sam 
+		double averageMarksOfSam = ( marksOfMaths + marksOfPhysics + marksofChemistry) / 3;
+		
+		// Display average marks of Sam 
+		System.out.println( "Average marks of Sam : " +averageMarksOfSam );
+		
+		
+	}
+}
