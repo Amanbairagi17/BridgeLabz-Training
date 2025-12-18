72ffea472998cbf698e48dd2a84487e19f8df716
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-18 23:34:51 +0530

    [Aman] : added program for finding multiplies using for loop

---
+++ gcr-codebase/control-flows/level-2/MultiplesUsingForLoop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MultiplesUsingForLoop{
+    public static void main(String[] args) {
+        // Creating Scanner object and taking input
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number : ");
+        int number = sc.nextInt();
+
+        // Check input
+        if (number <= 0 || number >= 100) {
+			System.out.println("Enter number between 1 to 100");
+			return;
+		}
+		// using for loop and printing multiples
+		for (int i = 99; i >= 1; i--) {
+            if (i % number == 0) {
+                System.out.println(i);
+            }
+        }
+		
+		sc.close();
+    }
+}
