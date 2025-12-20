a8aa9d7b50b8f7262fd46f52d01e2e1afa14cd85
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-20 19:56:12 +0530

    [Aman] : added program for utitlity unit convert I

---
+++ gcr-codebase/methods/level-2/UnitConvertor.java
@@ -0,0 +1,30 @@
+
+public class UnitConvertor{
+	// convert km to miles
+	public static double convertKmToMiles(double km) {
+       return km * 0.621371;
+    }
+
+    // Miles to km
+    public static double convertMilesToKm(double miles) {
+        return miles * 1.60934;
+    }
+
+    // Meters to feet
+    public static double convertMetersToFeet(double meters) {
+        return meters * 3.28084;
+    }
+
+    // Feet to meters
+    public static double convertFeetToMeters(double feet) {
+        return feet * 0.3048;
+    }
+	
+	/*public static void main(String[] args){
+		System.out.println(convertKmToMiles(10));
+		System.out.println(convertMilesToKm(10));
+		System.out.println(convertMetersToFeet(10));
+		System.out.println(convertFeetToMeters(10));
+		
+	}*/
+}
