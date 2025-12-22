f168939f5735c1bc77557f9a3f35425e2e64cfaf
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-22 15:03:57 +0530

    [Aman] : added comments in lower to upper and upper to lower program

--- gcr-codebase/strings/level-1/ConvertToLowercase.java
+++ gcr-codebase/strings/level-1/ConvertToLowercase.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 public class ConvertToUppercase{
+	//method for checking of two string is equal or not
 	private static boolean compare(String s1, String s2){
 		for(int i=0 ; i<s1.length() ; i++){
 			if(s1.charAt(i)  != s2.charAt(i)) return false;
   		}
 		return true;
 	}
+	
+	//method for converting lower to upper 
 	private static String convertIntoLowercase(String str){
 		String s = "";
 		for(int i=0 ; i<str.length() ; i++){
@@ -17,17 +20,18 @@ public class ConvertToUppercase{
 		return s;
 	}
 	public static void main(String[] args){
+		//creating object and taking input
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter string  : " );
 		String str = sc.next();
 		
+		//calling method 
 		String lowerToUpper1 = convertIntoLowercase(str);
 		String lowerToUpper2 = str.toLowerCase();
-		
-		
 		boolean isSame = compare(lowerToUpper1, lowerToUpper2);
 		
-		if(lowerToUpper1.equals(lowerToUpper2)) System.out.println("Both result are same, string equal ");
+		//printing result
+		if(isSame) System.out.println("Both result are same, string equal ");
 		else System.out.println("Both result are not same, string  are not equal ");
 		
 		
