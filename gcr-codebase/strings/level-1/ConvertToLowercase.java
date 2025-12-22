e1f379bc3d170ed658ba35cd856f8c389c6a26a1
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-22 14:44:16 +0530

    [Aman] : added program to convert uppercase to lowercase

---
+++ gcr-codebase/strings/level-1/ConvertToLowercase.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ConvertToUppercase{
+	private static boolean compare(String s1, String s2){
+		for(int i=0 ; i<s1.length() ; i++){
+			if(s1.charAt(i)  != s2.charAt(i)) return false;
+  		}
+		return true;
+	}
+	private static String convertIntoLowercase(String str){
+		String s = "";
+		for(int i=0 ; i<str.length() ; i++){
+			int ch = str.charAt(i);
+			if(ch >= 65 && ch <= 91) s +=  (char) (ch + 32) ;
+			else s += (char) ch;
+		}
+		return s;
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter string  : " );
+		String str = sc.next();
+		
+		String lowerToUpper1 = convertIntoLowercase(str);
+		String lowerToUpper2 = str.toLowerCase();
+		
+		
+		boolean isSame = compare(lowerToUpper1, lowerToUpper2);
+		
+		if(lowerToUpper1.equals(lowerToUpper2)) System.out.println("Both result are same, string equal ");
+		else System.out.println("Both result are not same, string  are not equal ");
+		
+		
+		System.out.println("The final string is : " +lowerToUpper1);
+
+	}
+}
