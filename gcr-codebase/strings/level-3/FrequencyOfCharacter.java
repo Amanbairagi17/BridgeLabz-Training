aa24552b0ad37bfb69f36c8d437991599b66ee2b
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-22 22:43:56 +0530

    [Aman] : added program to find frequency of each character

---
+++ gcr-codebase/strings/level-3/FrequencyOfCharacter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class FrequencyOfCharacter{
+	// return an array of string with 
+    static int[] getFrequency(String s) {
+		int[] freq = new int[256];
+        for(char ch : s.toCharArray()){
+			freq[(int)ch]++;
+		}
+		return freq;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String s = sc.next();
+		
+		// calling function to pring output
+        System.out.println(Arrays.toString(getFrequency(s)));
+    }
+}
