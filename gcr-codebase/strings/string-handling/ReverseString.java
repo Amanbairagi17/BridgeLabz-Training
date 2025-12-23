6fb12f070ddf1f65bc69f162b3b9c074b400f224
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-23 15:23:47 +0530

    [Aman] : added program to reverse string

---
+++ gcr-codebase/strings/string-handling/ReverseString.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReverseString{
+	//method to reverse string
+	private static String reverse(String str){
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=str.length()-1  ; i>=0 ; i--){
+			sb.append(str.charAt(i));
+		}
+		return sb.toString();
+	}
+	
+	public static void main(String[] args){
+		//creating object and taking input
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter string  : " );
+		String str = sc.nextLine();
+		int n = str.length();
+
+		//calling method and printing result
+		System.out.println("Reverse string is : " + reverse(str));
+
+	}
+}
