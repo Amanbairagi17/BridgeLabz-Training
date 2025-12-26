c3f77046f6fa188e2c2177816586c77c7a5a7ec0
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-26 23:57:28 +0530

    [Aman] : update book collection program

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/level1/HandBook.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/level1/HandBook.java
@@ -21,8 +21,8 @@ public class HandBook {
 		}
 		
 		public static void main(String[] args) {
-		HandBook book1= new HandBook("2States", "Chetan Bhagat", 500);
-		HandBook book2=new HandBook("Wings Of Fire","Abdul kalam.A.P.J",  500);
+		HandBook book1 = new HandBook("2States", "Chetan Bhagat", 500);
+		HandBook book2 = new HandBook("Wings Of Fire","Abdul kalam.A.P.J",  500);
 		book1.displayBookDetails();
 		book2.displayBookDetails();
 		}
