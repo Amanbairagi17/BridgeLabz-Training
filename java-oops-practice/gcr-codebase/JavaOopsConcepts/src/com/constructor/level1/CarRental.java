8c93b2c28ad5633f90e6fe11b0a794a2d331784d
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-27 16:34:26 +0530

    [Aman] : added program for calculating car cost

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/CarRental.java
@@ -0,0 +1,21 @@
+package com.constructor.level1;
+
+public class CarRental {
+	private String customerName;
+	private String carModel;
+	private int rentalDays;
+	
+	public CarRental(String customerName, String carModel, int rentalDays) {
+		this.customerName = customerName;
+		this.carModel = carModel;
+		this.rentalDays = rentalDays;
+	}
+	
+	public int getTotalPrice(){
+		return this.rentalDays * 100;
+	}
+	public static void main(String[] args) {
+		CarRental car = new CarRental("Customer1", "Model1", 10);
+		System.out.println("Total cost is : " +car.getTotalPrice());
+	}
+}
