18ac43b41421f1fbe6b38eea90721d15cf6676b3
Amanbairagi17 <amanbairagi741@gmail.com>
2025-12-27 17:20:59 +0530

    [Aman] : added program for library system

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/EBook.java
@@ -0,0 +1,13 @@
+package com.constructor.level1;
+
+public class EBook extends BookLibrary{
+	public static void main(String[] args) {
+		// creating object and accessing the properties of super class
+		EBook book = new EBook();
+		book.setAuthor("Authro1");
+		System.out.println(book.getAuthor());
+		book.ISBN = "Book12";
+		book.title = "Title of book";
+		book.display();
+	}
+}
