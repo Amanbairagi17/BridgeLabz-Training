f1559bececb1e3388913884e56f61f91792339e7
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-01 15:47:07 +0530

    [Aman] : added program for vehicle management using hybrid inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/vehiclemanagement/Vehicle.java
@@ -0,0 +1,18 @@
+package com.inheritance.hybridinheritance.vehiclemanagement;
+
+public class Vehicle {
+	//attribute
+	public int maxSpeed;
+    public String model;
+
+    // constructor
+    public Vehicle(int maxSpeed, String model) {
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+    }
+
+    // display
+    public void displayInfo() {
+        System.out.println(model + " | " + maxSpeed);
+    }
+}
