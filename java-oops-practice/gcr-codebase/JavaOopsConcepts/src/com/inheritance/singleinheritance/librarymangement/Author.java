73acbbd60fe6e4543851d7ed611f77384892600b
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-01 14:58:43 +0530

    [Aman] : added program for librarymanagement using single inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/singleinheritance/librarymangement/Author.java
@@ -0,0 +1,36 @@
+package com.inheritance.singleinheritance.librarymangement;
+
+public class Author extends Book {
+	//Attribute
+	private String name;
+	private String bio;
+	
+	// constructor
+	public Author(String title, int publicationYear, String name, String bio) {
+		super(title, publicationYear);
+		this.name = name;
+		this.bio = bio;
+	}
+	
+	// display info
+	public void display() {
+		System.out.println("Author : " + name + "Book title : " + super.getTitle());
+	}
+
+	// getter and setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}	
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/singleinheritance/librarymangement/DriverClass.java
@@ -0,0 +1,8 @@
+package com.inheritance.singleinheritance.librarymangement;
+
+public class DriverClass {
+	public static void main(String[] args) {
+		Author author = new Author("The Road", 1999, "Robert", "The author is robert");
+		author.display();
+	}
+}
