039e84c6a1b740d1e76b2a10a1388a5884bab26f
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-02 12:16:17 +0530

    [Aman] : added program for library management system using polymorphism, interface and abstarct class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/librarymanagementsystem/Book.java
@@ -0,0 +1,22 @@
+package com.encapsulationandpolymorphism.librarymanagementsystem;
+
+public class Book extends LibraryItem{
+	private boolean available = true;
+
+    // constructor
+    public Book(int itemId, String title, String author) {
+        super(itemId, title, author);
+    }
+
+    public int getLoanDuration() {
+        return 5;
+    }
+
+    public void reserveItem() {
+        available = false;
+    }
+
+    public boolean checkAvailability() {
+        return available;
+    }
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/librarymanagementsystem/DriverClass.java
@@ -0,0 +1,19 @@
+package com.encapsulationandpolymorphism.librarymanagementsystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DriverClass {
+	public static void main(String[] args) {
+    	// storing in a list of LibraryItem
+        List<LibraryItem> list = new ArrayList<>();
+        list.add(new Book(1, "Java Basics", "James"));
+        list.add(new Magazine(2, "Tech Monthly", "Editor"));
+        list.add(new DVD(3, "Learning Java", "Studio"));
+
+        for (LibraryItem item : list) {
+            item.getItemDetails();
+            System.out.println("duration : " + item.getLoanDuration() + ", available : " + item.checkAvailability());
+        }
+	}
+}
