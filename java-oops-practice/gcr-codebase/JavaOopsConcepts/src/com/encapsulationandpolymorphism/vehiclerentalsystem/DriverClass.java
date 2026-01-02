c82c013a6a219563c4e93c35071a6d686caf6a91
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-02 11:47:36 +0530

    [Aman] : added program for vehicle rental paltform using polymorphism, interface and abstarct class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/vehiclerentalsystem/DriverClass.java
@@ -0,0 +1,19 @@
+package com.encapsulationandpolymorphism.vehiclerentalsystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DriverClass {
+	public static void main(String[] args) {
+    	// storing in array list of Vehicle
+        List<Vehicle> list = new ArrayList<>();
+        list.add(new Car(0, 2000, "Car"));
+        list.add(new Bike(1, 800, "bike"));
+        list.add(new Truck(2, 5000, "truck"));
+
+        for (Vehicle v : list) {
+            System.out.println(v.getType() + " : " + v.calculateRentalCost(3));
+            v.getInsuranceDetails();
+        }
+    }
+}
