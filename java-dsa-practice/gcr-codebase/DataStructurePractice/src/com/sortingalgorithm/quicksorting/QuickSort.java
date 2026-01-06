fd1733a1e201ffe5ca3f2259b4b95807aa2d10c2
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-06 15:18:30 +0530

    [Aman] : added program for quick sort

---
+++ java-dsa-practice/gcr-codebase/DataStructurePractice/src/com/sortingalgorithm/quicksorting/QuickSort.java
@@ -0,0 +1,37 @@
+package com.sortingalgorithm.quicksorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+	public static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	public static int partition(int[] arr, int low, int high) {
+		int pivot = arr[high];
+		int i = low - 1;
+		
+		for(int j=low ; j<=high-1 ; j++) {
+			if(arr[j] < pivot) {
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i+1, high);
+		return i+1;
+	}
+	public static void quickSort(int[] arr, int low, int high) {
+		if(low < high) {
+			int pivot = partition(arr, low, high);
+			
+			quickSort(arr, low, pivot - 1);
+			quickSort(arr, pivot+1, high);
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr = {7,8,9,3,1,2};
+		quickSort(arr,0,arr.length-1);
+		System.out.println(Arrays.toString(arr));
+	}
+}
