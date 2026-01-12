2a30fa551463c4c3cf0a2c88d269e61aca0f1f42
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-12 20:30:02 +0530

    [Aman] : added scenarion based problem : 3 [Patient Navigation System]

---
+++ java-dsa-practice/scenario-based/ScenarioBasedOnDataStructure/src/com/emergencynavigationsystem/DriverClass.java
@@ -0,0 +1,21 @@
+package com.emergencynavigationsystem;
+
+public class DriverClass {
+	 public static void main(String[] args) {
+
+	        RoutingSystem system = new RoutingSystem();
+
+	        system.addUnit("Emergency", true);
+	        system.addUnit("Radiology", false);
+	        system.addUnit("Surgery", false);
+	        system.addUnit("ICU", true);
+
+	        System.out.println("Routing Patient:");
+	        system.routePatient();
+
+	        system.removeUnit("Emergency");
+
+	        System.out.println("Routing Patient:");
+	        system.routePatient();
+	    }
+}
