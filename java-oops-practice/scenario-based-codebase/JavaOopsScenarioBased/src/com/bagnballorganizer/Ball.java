1a5def0a95029cc5ff44f0d27e182fb94747cc67
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-22 08:01:19 +0530

    [Aman] : added program for ball n bag optimizer

---
+++ java-oops-practice/scenario-based-codebase/JavaOopsScenarioBased/src/com/bagnballorganizer/Ball.java
@@ -0,0 +1,32 @@
+package com.bagnballorganizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ball implements Storable {
+    private String id;
+    private String color;
+    private String size;
+
+    public Ball(String id, String color, String size) {
+        this.id = id;
+        this.color = color;
+        this.size = size;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String toString() {
+        return "Ball " + id + " " + color + " " + size;
+    }
+}
