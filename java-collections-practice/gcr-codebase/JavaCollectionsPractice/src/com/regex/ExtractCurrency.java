59ca16d0ddec0b6444ee548bcf02596a14c4f298
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-27 22:08:46 +0530

    [Aman] : added program for extract currency

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/regex/ExtractCurrency.java
@@ -0,0 +1,21 @@
+package com.regex;
+import java.util.regex.*;
+
+public class ExtractCurrency {
+
+    public static void main(String[] args) {
+
+        String text =
+            "The price is $45.99, and the discount is ₹10.50.";
+
+        String regex =
+            "[$₹€£]\\s?\\d+(\\.\\d{1,2})?";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
