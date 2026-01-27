278508ea3f795bfec9a6f8e61e0ccf04e68d3e3b
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-27 11:34:33 +0530

    [Aman] : added scenario based problem of binary search tree [University's Digital Record System]

---
+++ java-dsa-practice/scenario-based/ScenarioBasedOnDataStructure/src/com/tree/universitydigitalrecordsystem/DriverClass.java
@@ -0,0 +1,33 @@
+package com.tree.universitydigitalrecordsystem;
+
+public class DriverClass {
+	public static void main(String[] args) {
+
+        Tree tree = new Tree();
+
+        //  insertion
+        tree.insert(new Student(105, "Aman", "CSE"));
+        tree.insert(new Student(102, "Rohit", "ECE"));
+        tree.insert(new Student(110, "Neha", "IT"));
+        tree.insert(new Student(101, "Kunal", "ME"));
+        tree.insert(new Student(108, "Pooja", "CSE"));
+
+        // sorted display
+        tree.displaySorted();
+
+        // search
+        System.out.println("\nSearching roll 110:");
+        Student s = tree.search(110);
+        if (s != null)
+            System.out.println(s.rollNo + " " + s.name + " " + s.department);
+        else
+            System.out.println("Student not found");
+
+        //deletion
+        System.out.println("\nDeleting roll 102");
+        tree.delete(102);
+
+        //sorted after deletion
+        tree.displaySorted();
+    }
+}
---
+++ java-dsa-practice/scenario-based/ScenarioBasedOnDataStructure/src/com/tree/universitydigitalrecordsystem/Student.java
@@ -0,0 +1,13 @@
+package com.tree.universitydigitalrecordsystem;
+
+public class Student {
+    int rollNo;
+    String name;
+    String department;
+
+    Student(int rollNo, String name, String department) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.department = department;
+    }
+}
