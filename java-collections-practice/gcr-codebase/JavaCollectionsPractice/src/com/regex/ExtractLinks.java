94c0989a7c42c1ace3f28b101c60da45c08fe703
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-27 21:32:38 +0530

    [Aman] : added program for extract link

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/regex/ExtractLinks.java
@@ -0,0 +1,21 @@
+package com.regex;
+import java.util.regex.*;
+
+public class ExtractLinks {
+
+    public static void main(String[] args) {
+
+        String text =
+            "Visit https://www.google.com and http://example.org for more info.";
+
+        String regex =
+            "https?://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(/[^\\s]*)?";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
