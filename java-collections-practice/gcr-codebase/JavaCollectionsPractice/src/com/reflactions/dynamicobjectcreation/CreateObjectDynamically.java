2e206c1cf18aff5c1717933b204c090e7757c2fb
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-29 00:44:40 +0530

    [Aman] : added program to invoke dynamic mehtod at runtime using reflection

--- java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflactions/dynamicobjectcreation/CreateObjectDynamically.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflactions/dynamicobjectcreation/CreateObjectDynamically.java
@@ -1,20 +1,10 @@
 package com.reflactions.dynamicobjectcreation;
 
-class Ram {
-    private String name;
 
-    public Ram() {
-        this.name = "Aman";
-    }
-
-    public void display() {
-        System.out.println("Student Name: " + name);
-    }
-}
 public class CreateObjectDynamically {
     public static void main(String[] args) throws Exception {
 
-        Class<?> clazz = Class.forName("Ram");
+        Class<?> clazz = Class.forName("Student");
 
         // Create object dynamically
         Object obj = clazz.getDeclaredConstructor().newInstance();
