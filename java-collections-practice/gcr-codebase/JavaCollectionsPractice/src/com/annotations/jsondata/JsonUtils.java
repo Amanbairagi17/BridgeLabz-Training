0f1a06ca432bf02cce0a87f09f2a649fcbcfc096
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-29 10:17:17 +0530

    [Aman] : added program for json data

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/jsondata/JsonUtils.java
@@ -0,0 +1,53 @@
+package com.annotations.jsondata;
+
+import java.lang.reflect.Field;
+
+public class JsonUtils {
+
+    public static String toJson(Object obj) {
+
+        if (obj == null) {
+            return "null";
+        }
+
+        Class<?> clazz = obj.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        StringBuilder json = new StringBuilder();
+        json.append("{\n");
+
+        for (int i = 0; i < fields.length; i++) {
+
+            Field field = fields[i];
+            field.setAccessible(true);
+
+            try {
+                Object value = field.get(obj);
+
+                json.append("  \"")
+                    .append(field.getName())
+                    .append("\": ");
+
+                // String handling
+                if (value instanceof String || value instanceof Character) {
+                    json.append("\"").append(value).append("\"");
+                } else {
+                    json.append(value);
+                }
+
+                if (i < fields.length - 1) {
+                    json.append(",");
+                }
+
+                json.append("\n");
+
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        json.append("}");
+        return json.toString();
+    }
+}
+
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/jsondata/Student.java
@@ -0,0 +1,15 @@
+package com.annotations.jsondata;
+
+public class Student {
+    private int id;
+    private String name;
+    private double marks;
+    private boolean active;
+
+    public Student(int id, String name, double marks, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+        this.active = active;
+    }
+}
