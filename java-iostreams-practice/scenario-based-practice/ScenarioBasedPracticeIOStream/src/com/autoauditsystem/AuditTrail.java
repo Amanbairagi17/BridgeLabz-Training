6e253d554182a3fbb26f3eaeee6edd33cbb17461
Amanbairagi17 <amanbairagi741@gmail.com>
2026-02-09 11:39:35 +0530

    [Aman] : added program for EventTracker – Auto Audit System using annotations and reflactions

---
+++ java-iostreams-practice/scenario-based-practice/ScenarioBasedPracticeIOStream/src/com/autoauditsystem/AuditTrail.java
@@ -0,0 +1,9 @@
+package com.autoauditsystem;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuditTrail {
+    String action();
+}
---
+++ java-iostreams-practice/scenario-based-practice/ScenarioBasedPracticeIOStream/src/com/autoauditsystem/EventTracker.java
@@ -0,0 +1,36 @@
+package com.autoauditsystem;
+
+import java.lang.reflect.Method;
+import java.lang.annotation.*;
+import java.time.LocalDateTime;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class EventTracker {
+	public static void main(String[] args) {
+        JSONArray logs = new JSONArray();
+        try {
+            Class<?> cls = UserActions.class;
+            Method[] methods = cls.getDeclaredMethods();
+
+            for (Method m : methods) {
+                if (m.isAnnotationPresent(AuditTrail.class)) {
+                    AuditTrail audit = m.getAnnotation(AuditTrail.class);
+                    
+                    JSONObject log = new JSONObject();
+                    log.put("class", cls.getSimpleName());
+                    log.put("method", m.getName());
+                    log.put("action", audit.action());
+                    log.put("timestamp", LocalDateTime.now().toString());
+
+                    logs.put(log);
+                }
+            }
+            System.out.println(logs.toString());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
