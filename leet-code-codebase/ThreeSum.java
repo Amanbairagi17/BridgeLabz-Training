6e849360dca284cfff082a0370b224d585ab0f43
Amanbairagi17 <amanbairagi741@gmail.com>
2026-02-11 07:14:28 +0530

    [Aman] : added leetcode problem (15. 3Sum)

---
+++ leet-code-codebase/ThreeSum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i=0 ; i<n ; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+
+            while(j<k){
+                int ans = nums[i] + nums[j] + nums[k];
+
+                if(ans > 0){
+                    k--;
+                }else if(ans < 0){
+                    j++;
+                }else {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while(j<k && nums[j] == nums[j-1]){
+                         j++;
+                    }
+                }
+            }
+
+        }
+        return result;
+    }
+}
